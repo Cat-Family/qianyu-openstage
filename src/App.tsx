a8d5e5f868e41acd792ecaa36a4262aa3a620b6d
Jonas Lang <jonaslangdev@outlook.com>
2023-04-21 12:29:06 +0800

    ci(*): set env

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  Button,
+  Container,
+  Text,
+  Title,
+  Paper,
+  TextInput,
+  Group,
+  Anchor,
+  PasswordInput,
+  Checkbox
+} from '@mantine/core'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={theme => ({
+          fontFamily: `Greyliff CF, ${theme.fontFamily}`,
+          fontWeight: 900
+        })}
+      >
+        Welcome back!
+      </Title>
+      <Text color="dimmed" size="sm" align="center" mt={5}>
+        Do not have an account yet?{' '}
+        <Anchor size="sm" component="button">
+          Create account
+        </Anchor>
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Email" placeholder="you@exmple.com" required />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+        <Group position="apart" mt="lg">
+          <Checkbox label="Remember me" />
+          <Anchor component="button" size="sm">
+            Forgot password?
+          </Anchor>
+        </Group>
+        <Button fullWidth mt="xl">
+          Sign in
+        </Button>
+      </Paper>
+    </Container>
+  )
 }
 
-export default App;
+export default App
