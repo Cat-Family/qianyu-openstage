34b9909396a341cf5fc2731f63fa96e29a498a46
Jonas Lang <jonaslangdev@outlook.com>
2023-10-24 17:30:07 +0800

    feat: update table #10

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactElement, useState } from 'react';
+import { FC, ReactElement, ReactNode, useState } from 'react';
 import {
   Table as MantineTable,
   ScrollArea,
@@ -19,58 +19,63 @@ import {
   Badge,
   Anchor,
 } from '@mantine/core';
-import {
-  IconSelector,
-  IconChevronDown,
-  IconChevronUp,
-  IconSearch,
-  IconPencil,
-  IconDots,
-  IconMessages,
-  IconReportAnalytics,
-  IconNote,
-  IconTrash,
-} from '@tabler/icons-react';
+import { IconChevronDown, IconChevronUp, IconSearch, IconSelector } from '@tabler/icons-react';
 import classes from './Table.module.css';
+
+interface TableProps<T> {
+  columns: {
+    title: string;
+    sorted: boolean;
+    dataIndex: keyof T;
+    render?: (item: any) => ReactElement;
+  }[];
+  data: T[];
+}
 
 interface ThProps {
   children: React.ReactNode;
-  reversed: boolean;
+  reversed?: boolean;
   sorted: boolean;
-  onSort(): void;
+  onSort?(): void;
+  isSorted?: boolean;
 }
 
-function Th({ children, reversed, sorted, onSort }: ThProps) {
+function Th({ children, reversed, sorted, onSort, isSorted }: ThProps) {
   const Icon = sorted ? (reversed ? IconChevronUp : IconChevronDown) : IconSelector;
+
   return (
     <MantineTable.Th className={classes.th}>
-      <UnstyledButton onClick={onSort} className={classes.control}>
-        <Group justify="space-between">
-          <Text fw={500} fz="sm">
-            {children}
-          </Text>
-          <Center className={classes.icon}>
-            <Icon style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-          </Center>
-        </Group>
-      </UnstyledButton>
+      {isSorted ? (
+        <UnstyledButton onClick={onSort} className={classes.control}>
+          <Group justify="space-between">
+            <Text fw={500} fz="sm">
+              {children}
+            </Text>
+            <Center className={classes.icon}>
+              <Icon style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+            </Center>
+          </Group>
+        </UnstyledButton>
+      ) : (
+        <Text fw={500} fz="sm">
+          {children}
+        </Text>
+      )}
     </MantineTable.Th>
   );
 }
 
-const rolesData = ['Manager', 'Collaborator', 'Contractor', 'Designer', 'Engineer'];
-
-function filterData<T extends Object>(data: T[], search: string) {
+function filterData<T extends { id: string }>(data: T[], search: string) {
   const query = search.toLowerCase().trim();
   return data.filter((item) =>
-    keys(data[0]).some((key) => item[key]?.toString().toLowerCase().includes(query))
+    keys(data[0]).some((key) => String(item[key]).toLowerCase().includes(query))
   );
 }
 
-function sortData<T extends Object>(
+function sortData<T extends { id: string }>(
   data: T[],
   payload: { sortBy: keyof T | null; reversed: boolean; search: string }
-) {item
+) {
   const { sortBy } = payload;
 
   if (!sortBy) {
@@ -80,29 +85,28 @@ function sortData<T extends Object>(
   return filterData(
     [...data].sort((a, b) => {
       if (payload.reversed) {
-        return b[sortBy]?.toString().localeCompare(a[sortBy]?.toString());
+        return new String(b[sortBy]).localeCompare(String(a[sortBy]));
       }
 
-      return a[sortBy]?.toString().localeCompare(b[sortBy]?.toString());
+      return String(a[sortBy]).localeCompare(String(b[sortBy]));
     }),
     payload.search
   );
 }
 
-interface IProps<T> {
-  columns: {
-    title: string;
-    dataIndex: string;
-    render?: (item: any) => ReactElement;
-  }[];
-  data: T[];
-}
-
-export function Table<T extends object>({ columns, data }: IProps<T>) {
+function Table<T extends { id: string }>({ columns, data }: TableProps<T>) {
   const [search, setSearch] = useState('');
-  const [sortedData, setSortedData] = useState<T[]>(data);
+  const [sortedData, setSortedData] = useState(data);
   const [sortBy, setSortBy] = useState<keyof T | null>(null);
   const [reverseSortDirection, setReverseSortDirection] = useState(false);
+  const [selection, setSelection] = useState<string[]>([]);
+
+  const toggleRow = (id: string) =>
+    setSelection((current) =>
+      current.includes(id) ? current.filter((item) => item !== id) : [...current, id]
+    );
+  const toggleAll = () =>
+    setSelection((current) => (current.length === data.length ? [] : data.map((item) => item.id)));
 
   const setSorting = (field: keyof T) => {
     const reversed = field === sortBy ? !reverseSortDirection : false;
@@ -117,133 +121,81 @@ export function Table<T extends object>({ columns, data }: IProps<T>) {
     setSortedData(sortData(data, { sortBy, reversed: reverseSortDirection, search: value }));
   };
 
-  // const rows = sortedData.map((item) => (
-  //   <MantineTable.Tr key={item.name}>
-  //     <MantineTable.Td>
-  //       <Checkbox
-  //       // checked={selection.includes(item.id)} onChange={() => toggleRow(item.id)}
-  //       />
-  //     </MantineTable.Td>
-  //     <MantineTable.Td>
-  //       <Group gap="sm">
-  //         <Avatar size={40} src={item.avatar} radius={40} />
-  //         <Text fz="sm" fw={500}>
-  //           {item.name}
-  //         </Text>
-  //       </Group>
-  //     </MantineTable.Td>
-  //     <MantineTable.Td>
-  //       <Badge color={jobColors[item.job.toLowerCase()]} variant="light">
-  //         {item.job}
-  //       </Badge>
-  //     </MantineTable.Td>
-  //     <MantineTable.Td>
-  //       <Anchor component="button" size="sm">
-  //         {item.email}
-  //       </Anchor>
-  //     </MantineTable.Td>
-  //     <MantineTable.Td>
-  //       <Text fz="sm">${item.rate.toFixed(1)} / hr</Text>
-  //     </MantineTable.Td>
-  //     <MantineTable.Td>
-  //       <Group gap={0} justify="flex-end">
-  //         <ActionIcon variant="subtle" color="gray">
-  //           <IconPencil style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-  //         </ActionIcon>
-
-  //         <Menu
-  //           transitionProps={{ transition: 'pop' }}
-  //           withArrow
-  //           position="bottom-end"
-  //           withinPortal
-  //         >
-  //           <Menu.Target>
-  //             <ActionIcon variant="subtle" color="red">
-  //               <IconTrash style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-  //             </ActionIcon>
-  //           </Menu.Target>
-  //           <Menu.Dropdown>
-  //             <Menu.Item
-  //               leftSection={
-  //                 <IconMessages style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-  //               }
-  //             >
-  //               Send message
-  //             </Menu.Item>
-  //             <Menu.Item
-  //               leftSection={<IconNote style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
-  //             >
-  //               Add note
-  //             </Menu.Item>
-  //             <Menu.Item
-  //               leftSection={
-  //                 <IconReportAnalytics style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-  //               }
-  //             >
-  //               Analytics
-  //             </Menu.Item>
-  //             <Menu.Item
-  //               leftSection={<IconTrash style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
-  //               color="red"
-  //             >
-  //               Terminate contract
-  //             </Menu.Item>
-  //           </Menu.Dropdown>
-  //         </Menu>
-  //       </Group>
-  //     </MantineTable.Td>
-  //   </MantineTable.Tr>
-  // ));
-
   return (
-    <ScrollArea>
-      <Flex direction="column" wrap="nowrap" gap="lg">
-        <TextInput
-          placeholder="Search by any field"
-          leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
-          value={search}
-          // onChange={handleSearchChange}
-        />
-        <MantineTable mb="lg" horizontalSpacing="md" verticalSpacing="xs" miw={700} layout="fixed">
-          <MantineTable.Tbody>
-            <MantineTable.Tr>
-              <MantineTable.Th style={{ width: rem(40) }}>
-                <Checkbox
-                // onChange={toggleAll}
-                // checked={selection.length === data.length}
-                // indeterminate={selection.length > 0 && selection.length !== data.length}
-                />
-              </MantineTable.Th>
-              {columns.map((item, index) => (
+    <>
+      <TextInput
+        placeholder="Search by any field"
+        mb="md"
+        leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <MantineTable>
+        <MantineTable.Thead className="text-two">
+          <MantineTable.Tr>
+            <MantineTable.Td>
+              <Checkbox
+                onChange={toggleAll}
+                checked={selection.length === data.length}
+                indeterminate={selection.length > 0 && selection.length !== data.length}
+              />
+            </MantineTable.Td>
+            {columns.map((item, index) => (
+              <>
                 <Th
                   key={index}
-                  sorted={false}
+                  isSorted={!item.sorted}
+                  sorted={sortBy === item.dataIndex}
                   reversed={reverseSortDirection}
-                  onSort={() => setSorting<>(item)}
+                  onSort={() => setSorting(item.dataIndex)}
                 >
                   {item.title}
                 </Th>
-              ))}
-            </MantineTable.Tr>
-          </MantineTable.Tbody>
-          <MantineTable.Tbody>
-            {rows.length > 0 ? (
-              rows
-            ) : (
-              <MantineTable.Tr>
+              </>
+            ))}
+          </MantineTable.Tr>
+        </MantineTable.Thead>
+        <MantineTable.Tbody className="text-one overflow-auto text-center">
+          {sortedData.length > 0 ? (
+            sortedData.map((item, index) => (
+              <MantineTable.Tr key={index}>
                 <MantineTable.Td>
-                  <Text fw={500} ta="center">
-                    Nothing found
-                  </Text>
+                  <Checkbox
+                    checked={selection.includes(item.id)}
+                    onChange={() => toggleRow(item.id)}
+                  />
                 </MantineTable.Td>
+                {columns.map((column, index) => {
+                  console.log(typeof item[column.dataIndex]);
+
+                  if (column.render)
+                    return (
+                      <MantineTable.Td key={index}>
+                        {column.render(item[column.dataIndex])}
+                      </MantineTable.Td>
+                    );
+                  else
+                    return (
+                      <MantineTable.Td key={index}>
+                        {item[column.dataIndex]}
+                      </MantineTable.Td>
+                    );
+                })}
               </MantineTable.Tr>
-            )}
-          </MantineTable.Tbody>
-        </MantineTable>
-        <Center>
-          <Pagination total={20} value={100} withControls withEdges size="md" gap="sm" />
-        </Center>
-      </Flex>
-    </ScrollArea>
+            ))
+          ) : (
+            <MantineTable.Tr>
+              <MantineTable.Td colSpan={Object.keys(data[0]).length}>
+                <Text fw={500} ta="center">
+                  Nothing found
+                </Text>
+              </MantineTable.Td>
+            </MantineTable.Tr>
+          )}
+        </MantineTable.Tbody>
+      </MantineTable>
+    </>
   );
 }
+
+export { Table };
--- src/pages/Table/Table.page.tsx
+++ src/pages/Table/Table.page.tsx
@@ -2,15 +2,80 @@ import { Box, Container, Text, Title } from '@mantine/core';
 import { Table } from '../../components/Table';
 import classes from './Table.module.css';
 
+const deviceColumns = [
+  {
+    title: 'ID',
+    sorted: false,
+    dataIndex: 'id',
+  },
+  {
+    title: '设备名称',
+    sorted: false,
+    dataIndex: 'device',
+  },
+  {
+    title: '设备状态',
+    dataIndex: 'status',
+    sorted: false,
+    render: (item: number) =>
+      item === 1 ? (
+        <label style={{ color: 'rgb(0, 181, 101)' }}>作业中</label>
+      ) : item === 2 ? (
+        <label style={{ color: 'rgb(66, 160, 255)' }}>待机中</label>
+      ) : (
+        <label style={{ color: 'rgb(217, 35, 35)' }}>故障</label>
+      ),
+  },
+  {
+    title: '作业次数',
+    dataIndex: 'times',
+    sorted: false,
+  },
+  {
+    title: '操作',
+    dataIndex: 'operation',
+    sorted: false,
+    render: () => (
+      <div className="flex justify-center align-middle gap-2">
+        <button>edit</button>
+        <button>delete</button>
+      </div>
+    ),
+  },
+];
+const deviceData = [
+  {
+    id: '1',
+    device: '无人巡航机',
+    status: 1,
+    times: '1次',
+  },
+  {
+    id: '2',
+    device: '无人巡航机',
+    status: 2,
+    times: '1次',
+  },
+  {
+    id: '3',
+    device: '无人巡航机',
+    status: 3,
+    times: '1次',
+  },
+];
+
 const TablePage = () => {
   return (
     <>
-			<Box className={classes.header}>
-				<Title order={2}>TablePage</Title>
-				<Text size="sm">This is a table page</Text>
-			</Box>
+      <Box className={classes.header}>
+        <Title order={2}>TablePage</Title>
+        <Text size="sm">This is a table page</Text>
+      </Box>
       <Container p="md" size="lg">
-        {/* <Table /> */}
+        <Table<{ id: string; device: string; status: number; times: string }>
+          columns={deviceColumns}
+          data={deviceData}
+        />
       </Container>
     </>
   );
